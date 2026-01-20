d72b6138f95d03bdc010e60bb7d83b6a356a9a02
Jason Lee <jsnleeming@gmail.com>
2026-01-20 09:38:00 +0800

    Initial Commit

---
+++ gps-tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gps-tracker
+//
+//  Created by Jason on 2026/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gps-tracker/gps_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  gps_trackerApp.swift
+//  gps-tracker
+//
+//  Created by Jason on 2026/1/20.
+//
+
+import SwiftUI
+
+@main
+struct gps_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
